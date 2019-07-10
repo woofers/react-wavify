b457a427d68521722072bd6dfd6be04b4bf198e1
root <woofers1tt@gmail.com>
2019-07-10 05:21:12 +0000

    Updates

---
+++ precache-manifest.b66ecd97930418469bff9c01addb358b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf6a09e2ae6ff6bfc5fc3446b8c99ca",
+    "url": "/react-wavify/index.html"
+  },
+  {
+    "revision": "31cc405b93590074f2fc",
+    "url": "/react-wavify/static/js/2.4ef5195c.chunk.js"
+  },
+  {
+    "revision": "74f276322b69ecdf09c8",
+    "url": "/react-wavify/static/js/main.b98d8b6f.chunk.js"
+  },
+  {
+    "revision": "c3428c96d5b038f996a3",
+    "url": "/react-wavify/static/js/runtime~main.4ed62da1.js"
+  }
+]);
