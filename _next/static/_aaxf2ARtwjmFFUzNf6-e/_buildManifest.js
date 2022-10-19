84a6ed0ddea197c2f5340dc6deacc8cd442f29d5
woofers <woofers1tt@gmail.com>
2022-10-19 20:05:11 +0000

    Updates

---
+++ _next/static/_aaxf2ARtwjmFFUzNf6-e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-347fe64d1790c244.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
