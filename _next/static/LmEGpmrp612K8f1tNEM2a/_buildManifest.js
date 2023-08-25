1b3293c936e4b34a9a6fe33766086adeab0a043a
woofers <woofers1tt@gmail.com>
2023-08-25 05:32:02 +0000

    Updates

--- _next/static/LmEGpmrp612K8f1tNEM2a/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e8b35f8a0cf92802.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
