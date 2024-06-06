fbf6ace45683d252fcda37f51e7de1c742ec2253
woofers <woofers1tt@gmail.com>
2024-06-06 05:56:19 +0000

    Updates

--- _next/static/WNa2cM_25WMV_DnXePpAS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e87e5963ec1b8011.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/pages/_app-71995b7bcae0c9e0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{1597:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8781)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[774,179],()=>(n(1597),n(1732))),_N_E=_.O()}]);
