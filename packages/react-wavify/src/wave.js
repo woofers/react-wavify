61b1ef6486fbc3b8a610400f824a64f436f029c2
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-07-23 04:59:49 +0000

    Revert wave

--- packages/react-wavify/src/wave.js
+++ packages/react-wavify/src/wave.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 class Wave extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this._container = React.createRef()
     this.state = { path: '' }
@@ -11,33 +11,32 @@ class Wave extends Component {
     this._update = this._update.bind(this)
   }
 
-  _calculateWavePoints() {
+  _calculateWavePoints () {
     const points = []
-    for (let i = 0; i <= Math.max(this.props.points, 1); i++) {
+    for (let i = 0; i <= Math.max(this.props.points, 1); i ++) {
       const scale = 100
-      const x = (i / this.props.points) * this._width()
-      const seed =
-        (this._step + (i + (i % this.props.points))) * this.props.speed * scale
+      const x = i / this.props.points * this._width()
+      const seed = (this._step + (i + i % this.props.points)) * this.props.speed * scale
       const height = Math.sin(seed / scale) * this.props.amplitude
-      const y = Math.sin(seed / scale) * height + this.props.height
-      points.push({ x, y })
+      const y = Math.sin(seed / scale) * height  + this.props.height
+      points.push({x, y})
     }
     return points
   }
 
-  _buildPath(points) {
+  _buildPath (points) {
     let svg = `M ${points[0].x} ${points[0].y}`
     const initial = {
       x: (points[1].x - points[0].x) / 2,
-      y: points[1].y - points[0].y + points[0].y + (points[1].y - points[0].y)
+      y: (points[1].y - points[0].y) + points[0].y + (points[1].y - points[0].y)
     }
     const cubic = (a, b) => ` C ${a.x} ${a.y} ${a.x} ${a.y} ${b.x} ${b.y}`
     svg += cubic(initial, points[1])
     let point = initial
-    for (let i = 1; i < points.length - 1; i++) {
+    for (let i = 1; i < points.length - 1; i ++) {
       point = {
-        x: points[i].x - point.x + points[i].x,
-        y: points[i].y - point.y + points[i].y
+        x: (points[i].x - point.x) + points[i].x,
+        y: (points[i].y - point.y) + points[i].y
       }
       svg += cubic(point, points[i + 1])
     }
@@ -49,47 +48,47 @@ class Wave extends Component {
   _width = () => this._container.current.offsetWidth
   _height = () => this._container.current.offsetHeight
 
-  _redraw() {
+  _redraw () {
     this.setState({
       path: this._buildPath(this._calculateWavePoints())
     })
   }
 
-  _draw() {
+  _draw () {
     if (!this.props.paused) {
       const now = new Date()
-      this._elapsed += now - this._lastUpdate
+      this._elapsed += (now - this._lastUpdate)
       this._lastUpdate = now
     }
     const scale = 1000
-    this._step = (this._elapsed * Math.PI) / scale
+    this._step = this._elapsed * Math.PI / scale
     this._redraw()
   }
 
-  _update() {
+  _update () {
     this._draw()
     if (this._frameId) {
       this._resume()
     }
   }
 
-  _resume() {
+  _resume () {
     this._frameId = window.requestAnimationFrame(this._update)
     this._lastUpdate = new Date()
   }
 
-  componentDidMount() {
+  componentDidMount () {
     if (!this._frameId) {
       this._resume()
     }
   }
 
-  componentWillUnmount() {
+  componentWillUnmount () {
     window.cancelAnimationFrame(this._frameId)
     this._frameId = 0
   }
 
-  render() {
+  render () {
     const {
       style,
       className,
@@ -108,27 +107,11 @@ class Wave extends Component {
       ...rest
     } = this.props
     return (
-      <div
-        style={{ width: '100%', display: 'inline-block', ...style }}
-        className={className}
-        id={id}
-        ref={this._container}
-      >
-        <svg
-          width="100%"
-          height="100%"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          id={svgId}
-        >
+      <div style={{ width: '100%', display: 'inline-block', ...style }}
+           className={className} id={id} ref={this._container}>
+        <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" id={svgId}>
           {children}
-          <path
-            {...Object.assign(
-              {},
-              { d: this.state.path, fill, id: svgPathId },
-              rest
-            )}
-          />
+          <path {...Object.assign({}, { d: this.state.path, fill, id: svgPathId }, rest)} />
         </svg>
       </div>
     )
