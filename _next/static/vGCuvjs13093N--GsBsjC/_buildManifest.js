d94700de2d87c61d2e13fd867d40a5568d53ee28
woofers <woofers1tt@gmail.com>
2026-06-28 06:12:28 +0000

    Updates

---
+++ _next/static/vGCuvjs13093N--GsBsjC/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/react-wavify//_next/:path+",
+        "destination": "/react-wavify/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
